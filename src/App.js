0f074bfca954ebd65d8a371a834e1cd9a0b7e7f9
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-10-22 21:57:26 +0600

    Create Input Form react-bootstrap & Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import { getAuth } from "firebase/auth";
 import './App.css';
+// import RegisterBootstrap from "./components/RegisterBootstrap";
+import RegisterReactBootstrap from "./components/RegisterReactBootstrap";
 import app from './Firebase/firebase.init';
 
 const auth = getAuth(app);
 
-const handleRegister = (event) => {
-  event.preventDefault();
-  const email = event.target.email.value;
-  const password = event.target.password.value;
-  console.log(email, password);
-}
+// const handleRegister = (event) => {
+//   event.preventDefault();
+//   const email = event.target.email.value;
+//   const password = event.target.password.value;
+//   console.log(email, password);
+// }
 
-const handleEmailBlur = event => {
-  console.log(event.target.value);
-}
+// const handleEmailBlur = event => {
+//   console.log(event.target.value);
+// }
 
-const handlePasswordBlur = event => {
-  console.log(event.target.value);
-}
+// const handlePasswordBlur = event => {
+//   console.log(event.target.value);
+// }
 
 function App() {
   return (
-    <div className="App">
-      <form onSubmit={handleRegister}>
-        <input onChange={handleEmailBlur} type="email" name="email" id="" placeholder="Your Email" />
-        <br />
-        <input onChange={handlePasswordBlur} type="password" name="password" id="" placeholder="Your Password" />
-        <br />
-        <button type="submit">Register</button>
-      </form>
+    <div className="">
+      <RegisterReactBootstrap></RegisterReactBootstrap>
+      {/* <RegisterBootstrap></RegisterBootstrap> */}
     </div>
   );
 }
