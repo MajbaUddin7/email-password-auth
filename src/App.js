8a62acdda261f866173689d3d6af119c1600a99a
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-10-22 20:56:01 +0600

    Create normal form and input register

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import app from './Firebase/firebase.init';
 
 const auth = getAuth(app);
 
+const handleRegister = (event) => {
+  event.preventDefault();
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  console.log(email, password);
+}
+
+const handleEmailBlur = event => {
+  console.log(event.target.value);
+}
+
+const handlePasswordBlur = event => {
+  console.log(event.target.value);
+}
+
 function App() {
   return (
     <div className="App">
-      <form action="">
-        <input type="email" name="" id="" placeholder="Your Email" />
+      <form onSubmit={handleRegister}>
+        <input onChange={handleEmailBlur} type="email" name="email" id="" placeholder="Your Email" />
         <br />
-        <input type="password" name="" id="" placeholder="Your Password" />
+        <input onChange={handlePasswordBlur} type="password" name="password" id="" placeholder="Your Password" />
+        <br />
+        <button type="submit">Register</button>
       </form>
     </div>
   );
