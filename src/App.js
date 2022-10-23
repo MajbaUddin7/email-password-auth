470bb83de1de288e20003dbbca1ab4b45877c9ca
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-10-23 14:01:00 +0600

    firebase input data and input filed related another work done

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   return (
     <div className="">
       <RegisterReactBootstrap></RegisterReactBootstrap>
-      {/* <RegisterBootstrap></RegisterBootstrap> */}
+
     </div>
   );
 }
--- src/components/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import app from '../Firebase/firebase.init';
+
+
+const auth = getAuth(app);
 
 
 const RegisterReactBootstrap = () => {
 
+    const [passwordError, setpasswordError] = useState('');
+    const [success, setSuccess] = useState(false);
+
     const handleRegister = event => {
         event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+        setSuccess(false);
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
         console.log(email, password);
+
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setpasswordError('Please provide at least two uppercase');
+            return;
+
+        }
+        if (password.length < 6) {
+            setpasswordError('Please provide at least six charecter');
+            return;
+        }
+        if (!/(?=.*[!@#$&*])/.test(password)) {
+            setpasswordError('Please add at least one special character');
+            return;
+        }
+        setpasswordError('');
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setSuccess(true);
+                form.reset();
+            })
+            .catch(error => {
+                console.log('error', error);
+                setpasswordError(error.message);
+            })
     }
     return (
         <div className='w-50 mx-auto'>
@@ -17,13 +54,15 @@ const RegisterReactBootstrap = () => {
             <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Enter email" />
+                    <Form.Control type="email" name="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" placeholder="Password" />
+                    <Form.Control type="password" name="password" placeholder="Password" required />
                 </Form.Group>
+                <p className='text-danger'>{passwordError}</p>
+                {success && <p className='text-success'>User Created Successfully</p>}
                 <Button variant="primary" type="submit">
                     Register
                 </Button>
