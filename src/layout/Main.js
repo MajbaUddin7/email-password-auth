ebb63849d88baa8b79a65c17694f752f677d3a7b
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-10-25 13:30:23 +0600

    Final commit

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -4,10 +4,7 @@ import { Link, Outlet } from 'react-router-dom';
 const Main = () => {
     return (
         <div>
-            <nav>
-                <Link to='/login'>Login</Link>
-                <Link to='/register'>Register</Link>
-            </nav>
+            <h1 className='text-success'>My simple email Authentication system.</h1>
             <Outlet> </Outlet>
         </div>
     );
